ff1e843a49d708e246e1539396d4c758ce42ee23
Yu Yan <miracleyanyu@gmail.com>
2020-02-19 19:05:11 -0800

    Update WidgetService.java

--- myapp/src/main/java/com/example/myapp/services/WidgetService.java
+++ myapp/src/main/java/com/example/myapp/services/WidgetService.java
@@ -2,12 +2,16 @@ package com.example.myapp.services;
 
 import com.example.myapp.models.Widget;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.UUID;
 
 public class WidgetService {
 
   private List<Widget> widgetList = new ArrayList<Widget>();
+  private int order = 0;
 
   public List<Widget> findAllWidgets() {
     return widgetList;
@@ -34,29 +38,65 @@ public class WidgetService {
 
   public Widget createWidget(String tid, Widget widget) {
     widget.setTopicId(tid);
+    widget.setOrder(order++);
     widget.setId(UUID.randomUUID().toString());
     widgetList.add(widget);
+    widgetList.sort(Comparator.comparingInt(Widget::getOrder));
     return widget;
   }
 
   public int updateWidget(String wid, Widget newWidget) {
+    widgetList.sort(Comparator.comparingInt(Widget::getOrder));
+    boolean found = false;
+    boolean up = false;
+    boolean down = false;
     for (int i = 0; i < widgetList.size(); i++) {
-      if (widgetList.get(i).getId().equals(wid)) {
-        widgetList.set(i, newWidget);
+      if (found && !widgetList.get(i).getId().equals(wid) && up) {
+        widgetList.get(i).setOrder(widgetList.get(i).getOrder() + 1);
+        widgetList.sort(Comparator.comparingInt(Widget::getOrder));
         return 1;
       }
+      else if (found && !widgetList.get(i).getId().equals(wid) && down) {
+        widgetList.get(i).setOrder(widgetList.get(i).getOrder() - 1);
+        widgetList.sort(Comparator.comparingInt(Widget::getOrder));
+        return 1;
+      }
+      if (widgetList.get(i).getId().equals(wid)) {
+        if (widgetList.get(i).getOrder() > newWidget.getOrder()) {
+          up = true;
+        }
+        else if (widgetList.get(i).getOrder() < newWidget.getOrder()){
+          down = true;
+        }
+        widgetList.set(i, newWidget);
+        if (up) {
+          i = i - 2;
+        }
+        found = true;
+      }
     }
+    if (found) {
+      widgetList.sort(Comparator.comparingInt(Widget::getOrder));
+      return 1;
+    }
+    widgetList.sort(Comparator.comparingInt(Widget::getOrder));
     return 0;
   }
 
   public int deleteWidget(String wid) {
     boolean found = false;
-    for(Widget widget : widgetList) {
-      if (widget.getId().equals(wid)) {
-        widgetList.remove(widget);
+    widgetList.sort(Comparator.comparingInt(Widget::getOrder));
+    for(int i = 0; i < widgetList.size(); i++) {
+      if (found) {
+        widgetList.get(i).setOrder(widgetList.get(i).getOrder() - 1);
+      }
+      else if (widgetList.get(i).getId().equals(wid)) {
+        widgetList.remove(i);
+        i--;
         found = true;
       }
     }
+    order--;
     if (!found) {
       return 0;
     }
